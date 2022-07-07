7dd2aa547a89b35069bcf638205f0572f7f02e5c
adityathok <aditya0thok@gmail.com>
2022-07-07 12:25:20 +0700

    add editor style

--- inc/setup.php
+++ inc/setup.php
@@ -104,6 +104,11 @@ if ( ! function_exists( 'wpzaro_setup' ) ) {
 			)
 		);
 
+		// Add theme support editor-styles
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'css/theme.min.css' );
+		add_editor_style( 'css/custom-editor-style.min.css' );
+
 		// Set up the WordPress Theme logo feature.
 		add_theme_support( 'custom-logo' );
 
