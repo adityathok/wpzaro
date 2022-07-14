1867ac0e0a438901655b8519f4a41307f5ae6801
adityathok <aditya0thok@gmail.com>
2022-07-14 20:02:59 +0700

    change footer part

--- inc/builder-parts.php
+++ inc/builder-parts.php
@@ -45,29 +45,38 @@ if ( ! function_exists( 'wpzaro_header_layout_close' ) ) {
 if ( ! function_exists( 'wpzaro_footer_layout_open' ) ) {
     add_action('wpzaro_footer_open','wpzaro_footer_layout_open');
     function wpzaro_footer_layout_open() {
-        $container = get_theme_mod( 'wpzaro_container_type' );
         ?>
-        <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
         <!-- ******************* The Footer Area ******************* -->
-        <footer class="wrapper" id="wrapper-footer">
-
-            <div class="<?php echo esc_attr( $container ); ?>">
+        <footer class="wrapper p-0" id="wrapper-footer">
 
         <?php
     }
 }
 
-///closed footer layout
+///add footer site info
 if ( ! function_exists( 'wpzaro_footer_layout_content' ) ) {
     add_action('wpzaro_footer','wpzaro_footer_layout_content');
     function wpzaro_footer_layout_content() {
+        $container = get_theme_mod( 'wpzaro_container_type' );
         ?>
-            <div class="site-info">
 
-                <?php wpzaro_site_info(); ?>
+            <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-            </div><!-- .site-info -->
+            <div class="wrapper" id="wrapper-footer-site-info" role="footer">
+
+                <div class="<?php echo esc_attr( $container ); ?>">
+
+                    <div class="site-info">
+
+                        <?php wpzaro_site_info(); ?>
+
+                    </div><!-- .site-info -->
+
+                </div><!-- .container end -->
+
+            </div><!-- #wrapper-footer-site-info end -->
+
         <?php
     }
 }
@@ -78,9 +87,8 @@ if ( ! function_exists( 'wpzaro_footer_layout_close' ) ) {
     function wpzaro_footer_layout_close() {
         ?>
 
-            </div><!-- .container end -->
-
         </footer>  <!-- #wrapper-footer end -->
+
         <?php
     }
 }
