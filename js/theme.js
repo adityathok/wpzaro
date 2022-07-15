dfd63993df38619af6d9c78b22e9d60ce9f96583
adityathok <aditya0thok@gmail.com>
2022-07-15 14:19:18 +0700

    add js custom

--- js/theme.js
+++ js/theme.js
@@ -7173,5 +7173,27 @@
 	  }
 	})();
 
+	// Add your JS customizations here
+	jQuery(document).ready(function ($) {
+	  if ($(".wrapper-navbar-sticky").length) {
+	    const WrapperNavbar = $('#wrapper-navbar');
+	    var WrapperNavbarTop = WrapperNavbar.height();
+
+	    if ($("#wpadminbar").length) {
+	      WrapperNavbarTop = WrapperNavbarTop + $("#wpadminbar").height();
+	    }
+
+	    $(window).bind('scroll', function () {
+	      if ($(window).scrollTop() > WrapperNavbarTop) {
+	        $('body').addClass('body-navbar-scrolled');
+	        WrapperNavbar.addClass('wrapper-navbar-scrolled');
+	      } else {
+	        $('body').removeClass('body-navbar-scrolled');
+	        WrapperNavbar.removeClass('wrapper-navbar-scrolled');
+	      }
+	    });
+	  }
+	});
+
 }));
 //# sourceMappingURL=theme.js.map
