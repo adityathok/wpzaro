aefda28ccbd41e39bcf9c102bc78224200a5e837
adityathok <aditya0thok@gmail.com>
2023-07-26 20:46:56 +0700

    add Secondary Menu

--- inc/setup.php
+++ inc/setup.php
@@ -48,7 +48,8 @@ if (!function_exists('wpzaro_setup')) {
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'primary' => __('Primary Menu', 'wpzaro'),
+				'primary' 	=> __('Primary Menu', 'wpzaro'),
+				'secondary' => __('Secondary Menu', 'wpzaro'),
 			)
 		);
 
