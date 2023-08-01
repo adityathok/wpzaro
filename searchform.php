b41c6a9f533b76d8bf3c68917e5db528caf59d18
adityathok <aditya0thok@gmail.com>
2023-08-01 21:08:31 +0700

    update get_search_form

--- searchform.php
+++ searchform.php
@@ -21,7 +21,15 @@ if (isset($args['aria_label']) && !empty($args['aria_label'])) {
 	<label class="screen-reader-text" for="<?php echo $uid; ?>"><?php echo esc_html_x('Search for:', 'label', 'wpzaro'); ?></label>
 	<div class="input-group">
 		<input type="search" class="field search-field form-control" id="<?php echo $uid; ?>" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'wpzaro'); ?>">
-		<input type="submit" class="submit search-submit btn btn-dark" name="submit" value="<?php echo esc_attr_x('Search', 'submit button', 'wpzaro'); ?>">
+		<?php if (isset($args['aria_label']) && !empty($args['aria_label']) && $args['aria_label'] == 'icon') : ?>
+			<button class="btn btn-dark" type="submit">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+				</svg>
+			</button>
+		<?php else : ?>
+			<input type="submit" class="submit search-submit btn btn-dark" name="submit" value="<?php echo esc_attr_x('Search', 'submit button', 'wpzaro'); ?>">
+		<?php endif; ?>
 	</div>
 
 </form>
--- templates-parts/navbar-part/navbar-search.php
+++ templates-parts/navbar-part/navbar-search.php
@@ -15,14 +15,7 @@ $type_form  = wpzaro_theme_setting('wpzaro_searchform_header_type', 'dropdown');
 <?php if ($type_form == 'inline') : ?>
 
     <div class="header-search">
-        <form class="d-flex" role="search" method="GET" action="<?php echo esc_url(home_url('/')); ?>">
-            <input class="form-control me-2" name="s" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-dark" type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg>
-            </button>
-        </form>
+        <?php echo get_search_form(array('aria_label' => 'icon')); ?>
     </div>
 
 <?php elseif ($type_form == 'modal') : ?>
